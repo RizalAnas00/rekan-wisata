bb5851a9294b64abc1dc826ad5b32367148ccbce
azkanabhan <azkanabhansauqi@gmail.com>
2024-06-04 13:41:47 +0700

    Mengedit migrasi tabel jadwals

--- database/migrations/2024_05_24_161121_create_jadwals_table.php
+++ database/migrations/2024_05_24_161121_create_jadwals_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nama');
             $table->foreignId('wisata_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->date('tanggal_berangkat');
             $table->string('deskripsi');
             $table->timestamps();
